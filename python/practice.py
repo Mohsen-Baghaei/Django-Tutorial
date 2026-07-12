fbb929da19a1a3a2afd9b9c54d4ab1db68e776ac
Mohsen-Baghaei <mohsen.bhi99@gmail.com>
2026-07-12 12:29:27 +0330

    learning

---
+++ python/practice.py
@@ -0,0 +1,18 @@
+import random
+
+def odd_or_even(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+    
+print(odd_or_even(random.randint(0, 100)))
+
+def count_till_num(num):
+    total = 0
+    for i in range(1, num + 1):
+        total +=i
+    return total
+
+print(count_till_num(3))
+
