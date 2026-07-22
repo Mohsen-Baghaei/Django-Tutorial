483012abb435781d044fcf1a4e78db6e6999338d
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2026-07-22 15:28:35 +0330

    add a post through admin panel

--- Django/Lesson01/firstproject/blog/models.py
+++ Django/Lesson01/firstproject/blog/models.py
@@ -13,3 +13,6 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
