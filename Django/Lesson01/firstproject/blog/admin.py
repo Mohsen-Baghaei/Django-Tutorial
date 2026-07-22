fca0bf3354f96bdefb3ef22da898fc422f3021c8
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2026-07-22 15:46:01 +0330

    adding admin model

--- Django/Lesson01/firstproject/blog/admin.py
+++ Django/Lesson01/firstproject/blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-"
+    list_display = ("title", "counted_views", "status", "published_date") 
 admin.site.register(Post)
