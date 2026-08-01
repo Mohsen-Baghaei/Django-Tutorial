b20444eeaee1a8a23f6628026fea5347eb1379b2
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2026-08-01 23:53:18 +0330

    working on blog tags

--- Django/Lesson03/thirdproject/blog/templatetags/blog_tags.py
+++ Django/Lesson03/thirdproject/blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -8,7 +8,7 @@ def function():
     posts = Post.objects.filter(status=1).count()
     return posts
 
-@register.simple_tag(name="psots")
+@register.simple_tag(name="posts")
 def function2():
     posts = Post.objects.filter(status=1)
     return posts
@@ -16,3 +16,13 @@ def function2():
 @register.filter
 def snippet(value):
     return value[:100]
+
+@register.inclusion_tag("blog/popular-post.html")
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by("-published_date")[:3]
+    return {"posts": posts}
+
+@register.inclusion_tag("blog/post-categories.html")
+def post_categories():
+    categories = Category.objects.all()
+    return {"category": categories}
