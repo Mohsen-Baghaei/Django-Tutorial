0a885582c1639192409afbdda88aac3936649968
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2026-08-11 15:33:42 +0330

    finish main setup

--- Django/Lesson03/thirdproject/blog/urls.py
+++ Django/Lesson03/thirdproject/blog/urls.py
@@ -6,8 +6,8 @@ app_name = "blog"
 urlpatterns = [
     path("home/", views.blog_home, name="home"),
     path("post-<int:pid>/", views.blog_single, name="single"),
-    path("category/<str:cat_name>", views.blog_home, name="category"),
-    path("author/<str:author_username>", views.blog_home, name="author"),
+    path("category/<str:cat_name>/", views.blog_home, name="category"),
+    path("author/<str:author_username>/", views.blog_home, name="author"),
     path("search/", views.blog_search, name="search"),
     path("test/", views.test, name="test"),
 ]
--- Django/Lesson04/forthproject/forthproject/settings.py
+++ Django/Lesson04/forthproject/forthproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "website",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = "forthproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,6 +119,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
